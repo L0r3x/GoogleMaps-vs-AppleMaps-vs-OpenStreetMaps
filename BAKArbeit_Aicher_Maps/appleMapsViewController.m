9190bedcd6520dada76c010a255cced63de40aa2
L0r3x <aicher.michael@googlemail.com>
2014-02-06 23:36:36 +0100

    firstcode

---
+++ BAKArbeit_Aicher_Maps/appleMapsViewController.m
@@ -0,0 +1,29 @@
+//
+//  mapsSecondViewController.m
+//  BAKArbeit_Aicher_Maps
+//
+//  Created by Michael Aicher on 26.12.13.
+//  Copyright (c) 2013 FH Technikum Wien. All rights reserved.
+//
+
+#import "appleMapsViewController.h"
+
+@interface appleMapsViewController ()
+
+@end
+
+@implementation appleMapsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ BAKArbeit_Aicher_Maps/mapsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  mapsAppDelegate.h
+//  BAKArbeit_Aicher_Maps
+//
+//  Created by Michael Aicher on 26.12.13.
+//  Copyright (c) 2013 FH Technikum Wien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mapsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
